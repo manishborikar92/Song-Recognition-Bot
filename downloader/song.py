f43d00ccfaab6fd73223e572b426f0af027efb68
Manish Borikar <manishborikar92@gmail.com>
2024-12-31 02:15:48 +0530

    Updated code structure

--- downloader/song.py
+++ downloader/song.py
@@ -37,7 +37,7 @@ def download_song(title, artist):
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
-                'preferredquality': '320',
+                'preferredquality': '192',
             }],
             'outtmpl': os.path.join(output_dir, f'{sanitized_title}.%(ext)s'),
             'quiet': True,  # Reduce console output
